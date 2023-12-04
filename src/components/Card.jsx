5125af60a80e63950e19b68c721db96a265443a4
zairzabar <zuber.ansari.997@gmail.com>
2023-12-05 03:45:41 +0530

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,44 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { FaMapLocationDot } from "react-icons/fa6";
 
-function Card(props) {
-  console.log(props.coverImg);
+function Card({
+  city,
+  country,
+  description,
+  durationFrom,
+  durationTo,
+  img,
+  mapLink,
+}) {
+  console.log(img);
   return (
     <div className="card__container">
       <div className="img__container">
-        <img src={`/images/delhi.jpg`} alt={props.city} />
+        <img src={`images/${img}`} alt={city} />
       </div>
       <div className="card__details">
         <div className="location">
           <FaLocationDot className="icon__red" />
-          <h3>{props.country}</h3>
-          <a href={props.mapLink}>View on Google Maps</a>
+          <h3>{country}</h3>
+          <a href={mapLink}>View on Google Maps</a>
           <FaMapLocationDot className="icon__red" />
         </div>
-        <h2 className="city">{props.city}</h2>
+        <h2 className="city">{city}</h2>
         <small className="duration">
-          {props.duration.from} - {props.duration.to}
+          {durationFrom} - {durationTo}
         </small>
-        <p className="card__desc">{props.description}</p>
+        <p className="card__desc">{description}</p>
       </div>
     </div>
   );
 }
 
 export default Card;
+
+// city: "Ooty";
+// country: "India";
+// description: "Ooty, short for Ootacamund and officially Udhagamandalam, is a popular hill resort in the state of Tamil Nadu in Southern India. The name Udhagamandalam is derived from the Toda word 'othakal-mund' which means 'house in mountains'.";
+// durationFrom: "Novemver 2018";
+// durationTo: "December 2018";
+// img: "ooty";
+// mapLink: "https://maps.app.goo.gl/UBaA8VHrGD7T7nEN7";
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,16 @@ function Main() {
   return (
     <main className="main">
       {data.map((item) => (
-        <Card key={item.id} {...item} />
+        <Card
+          key={item.id}
+          img={item.coverImg}
+          country={item.country}
+          city={item.city}
+          description={item.description}
+          mapLink={item.mapLink}
+          durationFrom={item.duration.from}
+          durationTo={item.duration.to}
+        />
       ))}
     </main>
   );
